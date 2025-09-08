71e0bb8bfe177d57e6bcbcd2565559772ade3bc8
Vitalii Budnik <nekrich@users.noreply.github.com>
2025-09-08 17:08:14 +0300

    fix: compiler warnings

--- Package.swift
+++ Package.swift
@@ -146,7 +146,7 @@ enum Targets {
           name: "SemanticVersionMacroPlugin",
           dependencies: [
             .target(name: "SemanticVersion"), .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
-            .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
+            .product(name: "SwiftCompilerPlugin", package: "swift-syntax"), .product(name: "SwiftSyntax", package: "swift-syntax"),
           ]
         ),
         .plugin(
