6a36f800491818c4f80dd1ec1eb4196cf327bf5d
Vitalii Budnik <nekrich@users.noreply.github.com>
2025-09-16 15:58:06 +0300

    chore: exetuable linux binary

--- scripts/linux-container-actions/build.sh
+++ scripts/linux-container-actions/build.sh
@@ -9,6 +9,9 @@ prepare_package_copy
 echo "Building Release configuration..."
 ./scripts/tools/swift/swift.sh --action=build --configuration=release
 
+echo "Making built binary executable..."
+chmod +x .build/aarch64-unknown-linux-gnu/release/mpct
+
 echo "Copying built release binary back to the original package directory..."
 cp .build/aarch64-unknown-linux-gnu/release/mpct /package/.build/aarch64-unknown-linux-gnu/release/mpct
 
